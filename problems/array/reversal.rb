5bfea7fe76d6f939d7f7ae6b8b1f8785e760f838
Marco Castro <marcodotcastro@gmail.com>
2021-04-22 07:28:00 -0300

    feat(array): add reversal

---
+++ problems/array/reversal.rb
@@ -0,0 +1,15 @@
+class ProblemsArray
+  def reversal(array, k)
+    new_array = []
+
+    new_array << array.pop(k)
+    new_array << array
+
+    new_array.flatten!
+  end
+end
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+k = 3
+
+p ProblemsArray.new.reversal(array, k)
